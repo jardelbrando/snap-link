905767f2517a2ded459b0f6b2a348a12f6262bed
Jardel <jardelbrando@gmail.com>
2026-06-12 16:59:05 -0300

    Setting the entities.

---
+++ src/main/java/com/snaplink/api/domain/Url.java
@@ -0,0 +1,47 @@
+package com.snaplink.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(
+        name = "urls",
+        indexes = {
+                @Index(name = "idx_url_short_code", columnList = "short_code")
+        }
+)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "url_sequence")
+    @SequenceGenerator(name = "url_sequence", sequenceName = "url_sequence", allocationSize = 1)
+    @Column(name = "url_id")
+    private Long id;
+
+    @Column(name = "original_url", nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(name = "short_code", nullable = false, unique = true, length = 10)
+    private String shortCode;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "url", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ClickLog> clickLogs = new ArrayList<>();
+}
---
+++ src/main/java/com/snaplink/api/domain/User.java
@@ -0,0 +1,38 @@
+package com.snaplink.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Url> urls = new ArrayList<>();
+}
